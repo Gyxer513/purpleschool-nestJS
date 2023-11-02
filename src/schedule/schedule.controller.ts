63702846163d9b566e34a85e2b31c4143724c014
Gyxer513 <fpanchuk@yandex.ru>
2023-11-02 15:28:15 +0300

    added telegram logik

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -16,12 +16,14 @@ import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { RoomsService } from '../rooms/rooms.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { RolesGuard } from 'src/roles/roles.guard';
+import { TelegramService } from 'src/telegram/telegram.service';
 
 @Controller('schedule')
 export class ScheduleController {
   constructor(
     private readonly scheduleService: ScheduleService,
     private readonly roomsService: RoomsService,
+    private readonly telegramServise: TelegramService,
   ) {}
 
   @UsePipes(new ValidationPipe())
@@ -30,7 +32,13 @@ export class ScheduleController {
     const { roomId } = createScheduleDto;
     const room = await this.roomsService.findById(roomId);
     if (room) {
+      const message = `Комната с Id +
+       ${createScheduleDto.roomId}\n +
+       забронирована на ${createScheduleDto.date}\n`;
+      this.telegramServise.sendMessage(message);
+
       return this.scheduleService.create(createScheduleDto);
+      
     } else {
       throw new HttpException(
         `Комната с id: ${roomId} не найдена`,
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Telegraf } from 'telegraf';
+import { ITelegramOptions } from './telegram.interface';
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+  bot: Telegraf;
+  options: ITelegramOptions;
+
+  constructor() {
+    this.options = {
+      chatId: '',
+      token: '',
+    };
+    this.bot = new Telegraf(this.options.token);
+  }
+
+  async sendMessage(message: string, chatId: string = this.options.chatId) {
+    await this.bot.telegram.sendMessage(chatId, message);
+  }
+}
