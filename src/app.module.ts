e4f9a24e96c25c9e198774f40e4eee8c6291ce4f
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-11-05 14:14:22 +0300

    fix review

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from './schedule/schedule.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { path } from 'app-root-path';
 import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 
 @Module({
   imports: [
@@ -25,7 +26,11 @@ import { TelegramModule } from './telegram/telegram.module';
       rootPath: `${path}/uploads`,
       serveRoot: '/static'
     }),
-    TelegramModule,
+    TelegramModule.foRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig
+    }),
   ],
   controllers: [],
   providers: [],
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Telegraf } from 'telegraf';
 import { ITelegramOptions } from './telegram.interface';
+import { TELEGRAM_MODULE_OPTIONS } from './telegram.constants';
 
 @Injectable()
 export class TelegramService {
   bot: Telegraf;
   options: ITelegramOptions;
 
-  constructor() {
-    this.options = {
-      chatId: '',
-      token: '',
-    };
-    this.bot = new Telegraf(this.options.token);
+  constructor(
+    @Inject(TELEGRAM_MODULE_OPTIONS) options: ITelegramOptions
+  ) {
+    this.bot = new Telegraf(options.token);
+    this.options = options;
   }
 
   async sendMessage(message: string, chatId: string = this.options.chatId) {
